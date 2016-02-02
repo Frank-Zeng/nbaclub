619ae81b00c4d3a66fc8fb49d20accdc93891906
Frank Zeng <zengfgouyt@outlook.com>
2016-02-02 21:52:35 +0800

    add

---
+++ ClubMall/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ClubMall.Startup))]
+namespace ClubMall
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
